4d3509e81bd434d7aebf84457cfdebb5f90a51f5
andri <andri@AndrisPC>
2014-10-13 20:19:09 +0200

    -test

--- src/ch/hsr/schatzkarte/MainActivity.java
+++ src/ch/hsr/schatzkarte/MainActivity.java
@@ -1,9 +1,25 @@
 package ch.hsr.schatzkarte;
 
+import java.io.File;
+
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.MapTileProviderBase;
+import org.osmdroid.tileprovider.modules.IArchiveFile;
+import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
 
 public class MainActivity extends Activity {
 
@@ -11,24 +27,35 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
+		MapView map = (MapView) findViewById(R.id.mapview );
+		map.setTileSource(TileSourceFactory.MAPQUESTOSM);
+		 
+		map.setMultiTouchControls(true);
+		map.setBuiltInZoomControls(true);
+		 
+		IMapController controller = map.getController();
+		controller.setZoom(18);
+		 
+		// Die TileSource beschreibt die Eigenschaften der Kacheln die wir anzeigen
+		XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", ResourceProxy.string.offline_mode, 1, 20, 256, ".png", "http://example.org/");
+		 
+		File file = new File(Environment.getExternalStorageDirectory() /* entspricht /sdcard/ */, "hsr.mbtiles");
+		 
+		/* Das verwenden von mbtiles ist leider ein wenig aufwändig, wir müssen
+		 * unsere XYTileSource in verschiedene Klassen 'verpacken' um sie dann
+		 * als TilesOverlay über der Grundkarte anzuzeigen.
+		 */
+		MapTileModuleProviderBase treasureMapModuleProvider = new MapTileFileArchiveProvider(new SimpleRegisterReceiver(this), 
+				treasureMapTileSource, new IArchiveFile[] { MBTilesFileArchive.getDatabaseFileArchive(file) });
+		 
+		MapTileProviderBase treasureMapProvider = new MapTileProviderArray(treasureMapTileSource, null,
+				new MapTileModuleProviderBase[] { treasureMapModuleProvider });
+		 
+		TilesOverlay treasureMapTilesOverlay = new TilesOverlay(treasureMapProvider, getBaseContext());
+		treasureMapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
+		 
+		// Jetzt können wir den Overlay zu unserer Karte hinzufügen:
+		map.getOverlays().add(treasureMapTilesOverlay);	
 	}
 }
